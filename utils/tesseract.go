eb350fa860c7de0af933b977ee22c4021740d9d2
ben@ryben.dev <ben@ryben.dev>
2022-06-11 03:11:46 -0500

    fix: tesseract folder searching

--- utils/tesseract.go
+++ utils/tesseract.go
@@ -7,23 +7,22 @@ import (
 	"syscall"
 )
 
+var tesseractPath = ""
+
 func init() {
 	println("Finding tesseract location...")
 	programFiles, err := exec.Command("cmd", "/C", "echo %PROGRAMFILES%").Output()
 	if err != nil {
 		panic(err)
 	}
-	tesseractPath := strings.TrimSuffix(string(programFiles), "\r\n") + "\\Tesseract-OCR\\tesseract.exe"
+	tesseractPath = strings.TrimSuffix(string(programFiles), "\r\n") + "\\Tesseract-OCR"
 	println("Tesseract:", tesseractPath)
-	println("Appending tesseract to PATH...")
-	if err = exec.Command("cmd", "/C", "SET PATH=%PATH%;"+tesseractPath).Run(); err != nil {
-		panic(err)
-	}
 }
 
 func Tesseract(img []byte) (string, error) {
 	child := exec.Command("cmd")
 	child.SysProcAttr = &syscall.SysProcAttr{CmdLine: `/C tesseract stdin - -l eng`}
+	child.Dir = tesseractPath
 	child.Stdin = bytes.NewReader(img)
 	out, err := child.CombinedOutput()
 	if err != nil {
